c59835eb7b313861bfcce2389bb24f7aa54f9a39
Clooooode <jackey8616@gmail.com>
2023-03-30 14:12:38 +0800

    feat: apply generate cross server token method

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,14 +1,19 @@
 import { Body, Controller, Post } from "@nestjs/common";
 
+import { AuthService } from "./auth.service";
 import { CrossServerTokenDTO } from "./auth.type";
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private readonly authService: AuthService,
+  ) {}
+
   @Post('token')
   async generateCrossServerToken(
     @Body() body: CrossServerTokenDTO,
   ) {
-    const authToken = '';
+    const authToken = await this.authService.generateCrossServerToken(body);
     return {
       code: 'SUCCESS',
       message: 'get auth token successfully',
