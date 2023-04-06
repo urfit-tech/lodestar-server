a37140422a0956208d7d018d925d1b49442d1fc8
Clooooode <jackey8616@gmail.com>
2023-04-06 23:56:34 +0800

    chore: print __dirname

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,7 +4,7 @@ import { DataSourceOptions, DataSource } from 'typeorm'
 import { env } from 'process';
 
 config({ path: `.env${env.NODE_ENV ? `.${env.NODE_ENV}` : ''}`});
-
+console.log(__dirname);
 export const AppDataSourceConfig: DataSourceOptions = {
   type: "postgres",
   url: process.env.DB_URI,
