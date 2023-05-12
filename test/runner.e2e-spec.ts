e4204fac097c7510b1046f94d2fb0e49a449a8ea
Clooooode <jackey8616@gmail.com>
2023-05-12 09:45:37 +0800

    feat: add dayjs package & change import setup

--- test/runner.e2e-spec.ts
+++ test/runner.e2e-spec.ts
@@ -1,4 +1,4 @@
-import * as request from 'supertest';
+import request from 'supertest';
 import { Injectable, Logger } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 
