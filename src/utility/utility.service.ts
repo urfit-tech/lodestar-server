ed0d622369379913b064ac3d2928844406f59229
Clooooode <jackey8616@gmail.com>
2023-05-17 20:18:28 +0800

    feat: add UtilityService

--- src/utility/utility.service.ts
+++ src/utility/utility.service.ts
@@ -1,4 +1,12 @@
+import { createCipheriv } from 'crypto'
 import { Injectable } from '@nestjs/common'
 
 @Injectable()
-export class UtilityService {}
+export class UtilityService {
+  encrypt(hashKey: string, iv: string, data: string): string {
+    const cipher = createCipheriv('aes-256-cbc', hashKey, iv);
+    let encrypted = cipher.update(data, 'utf8', 'hex');
+    encrypted += cipher.final('hex');
+    return encrypted;
+  }
+}
