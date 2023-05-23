8b5030fdd55604634af54850d224bfb071b3bdc2
ponchimeow <trancetoradora@gmail.com>
2023-05-23 20:44:02 +0800

    feat: init cf api

--- src/media/video/video.controller.ts
+++ src/media/video/video.controller.ts
@@ -1,16 +1,14 @@
-import { Controller, Headers, Param, Post } from '@nestjs/common';
+import { Controller, Headers, Param, Post, Get, Body } from '@nestjs/common';
 
 import { VideoService } from './video.service';
-import { VideoTokenDTO } from './video.dto';
+import { DownloadableFileDTO, VideoTokenDTO } from './video.dto';
 
 @Controller({
   path: 'videos',
   version: ['1', '2'],
 })
 export class VideoController {
-  constructor(
-    private readonly videoService: VideoService,
-  ) {}
+  constructor(private readonly videoService: VideoService) {}
 
   @Post(':videoId/token')
   async getCfToken(
@@ -19,13 +17,45 @@ export class VideoController {
   ): Promise<{ code: string; message: string; result: VideoTokenDTO }> {
     const [_, token] = authorization ? authorization.split(' ') : [undefined, undefined];
 
-    const { videoToken, cfOptions } = await this.videoService.generateCfVideoToken(
-      videoId, token,
-    );
+    const { videoToken, cfOptions } = await this.videoService.generateCfVideoToken(videoId, token);
     return {
       code: 'SUCCESS',
       message: 'successfully sign the url',
       result: { token: videoToken, cloudflareOptions: cfOptions },
+    };
+  }
+
+  @Get(':videoId/download')
+  async getCloudflareDownloadableFile(
+    @Param('videoId') videoId: string,
+  ): Promise<{ code: string; message: string; result: DownloadableFileDTO }> {
+    const { status, url, percentComplete } = await this.videoService.generateCloudFlareDownloadableFile(videoId);
+
+    if (status !== 'ready' && percentComplete !== 100) {
+      return {
+        code: 'SUCCESS',
+        message: 'file still processing',
+        result: { status, url, percentComplete },
+      };
+    }
+
+    return {
+      code: 'SUCCESS',
+      message: 'successfully get downloadable file',
+      result: { status, url, percentComplete },
+    };
+  }
+
+  @Post(':videoId/download')
+  async generateCloudflareDownloadableFile(
+    @Param('videoId') videoId: string,
+  ): Promise<{ code: string; message: string; result: DownloadableFileDTO }> {
+    const { status, url, percentComplete } = await this.videoService.generateCloudFlareDownloadableFile(videoId);
+
+    return {
+      code: 'SUCCESS',
+      message: 'successfully generate downloadable file',
+      result: { status, url, percentComplete },
     };
   }
 }
--- src/media/video/video.dto.ts
+++ src/media/video/video.dto.ts
@@ -3,4 +3,10 @@ import { CfVideoStreamOptions } from './video.type';
 export class VideoTokenDTO {
   token: string;
   cloudflareOptions: CfVideoStreamOptions;
-};
+}
+
+export class DownloadableFileDTO {
+  status: string;
+  url: string;
+  percentComplete: number;
+}
