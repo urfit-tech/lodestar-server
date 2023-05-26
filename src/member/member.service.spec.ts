7d2c0ca655f3fd922838895b96aa22320f92569b
Clooooode <jackey8616@gmail.com>
2023-05-27 02:25:02 +0800

    test: cases for unknown items
    Unknown columns in header.
    Unknown value with extra columns in data.

--- src/member/member.service.spec.ts
+++ src/member/member.service.spec.ts
@@ -192,5 +192,132 @@ describe('MemberService', () => {
       expect(member.star).toBe(999);
       expect(member.createdAt).toBe(createdAt);
     });
+
+    it('Should skip unknown categories, properties, tags', async () => {
+      const memberId = v4();
+      const createdAt = new Date().toISOString();
+      const rawRows = [
+        {
+          '流水號': 'id',
+          '姓名': 'name',
+          '帳號': 'username',
+          '信箱': 'email',
+          '手機1': 'phones.0',
+          '手機2': 'phones.1',
+          '分類1': 'categories.0',
+          '不存在分類2': 'categories.1',
+          '屬性1': 'properties.0',
+          '不存在屬性2': 'properties.1',
+          '標籤1': 'tags.0',
+          '不存在標籤2': 'tags.1',
+          '星等': 'star',
+          '建立日期': 'createdAt',
+        },
+        {
+          '流水號': memberId,
+          '姓名': 'test_not_exists',
+          '帳號': 'test_not_exists_account',
+          '信箱': 'test_not_exists_email@test.com',
+          '手機1': '0912345678',
+          '手機2': '',
+          '分類1': 'test_category1',
+          '不存在分類2': 'not_exists_category',
+          '屬性1': 'test_property1',
+          '不存在屬性2': 'not_exists_property',
+          '標籤1': 'test_tag1',
+          '不存在標籤2': 'not_exists_tag',
+          '星等': '999',
+          '建立日期': createdAt,
+        },
+      ];
+      mockDefinitionInfra.getCategories.mockReturnValueOnce([
+        { id: 'test_category1_id', name: 'test_category1' },
+      ]);
+      mockDefinitionInfra.getProperties.mockReturnValueOnce([
+        { id: 'test_property1_id', name: '屬性1' },
+      ])
+      mockDefinitionInfra.getTags.mockReturnValueOnce([
+        { name: 'test_tag1' },
+      ]);
+
+      const [member] = await service.rawCsvToMember('test-app-id', rawRows);
+      expect(member.id).toBe(memberId);
+      expect(member.name).toBe('test_not_exists');
+      expect(member.username).toBe(memberId);
+      expect(member.email).toBe('test_not_exists_email@test.com');
+      expect(member.memberPhones.length).toBe(1);
+      expect(member.memberPhones[0].phone).toEqual('0912345678');
+      expect(member.memberCategories.length).toBe(1);
+      expect(member.memberCategories[0].category.id).toEqual('test_category1_id');
+      expect(member.memberProperties.length).toBe(1);
+      expect(member.memberProperties[0].property.id).toEqual('test_property1_id');
+      expect(member.memberProperties[0].value).toEqual('test_property1');
+      expect(member.memberTags.length).toBe(1);
+      expect(member.memberTags[0].tagName2.name).toEqual('test_tag1');
+      expect(member.star).toBe(999);
+      expect(member.createdAt).toBe(createdAt);
+    });
+
+    it('Should skip extra unknown categories, properties, tags', async () => {
+      const memberId = v4();
+      const createdAt = new Date().toISOString();
+      const rawRows = [
+        {
+          '流水號': 'id',
+          '姓名': 'name',
+          '帳號': 'username',
+          '信箱': 'email',
+          '手機1': 'phones.0',
+          '手機2': 'phones.1',
+          '分類1': 'categories.0',
+          '屬性1': 'properties.0',
+          '標籤1': 'tags.0',
+          '星等': 'star',
+          '建立日期': 'createdAt',
+        },
+        {
+          '流水號': memberId,
+          '姓名': 'test_extra_unknown',
+          '帳號': 'test_extra_unknown_account',
+          '信箱': 'test_extra_unknown_email@test.com',
+          '手機1': '0912345678',
+          '手機2': '',
+          '分類1': 'test_category1',
+          '多於分類1': 'extra_unknown_category',
+          '屬性1': 'test_property1',
+          '多餘屬性2': 'extra_unknown_property',
+          '標籤1': 'test_tag1',
+          '多餘標籤2': 'extra_unknown_tag',
+          '星等': '999',
+          '建立日期': createdAt,
+        },
+      ];
+      mockDefinitionInfra.getCategories.mockReturnValueOnce([
+        { id: 'test_category1_id', name: 'test_category1' },
+      ]);
+      mockDefinitionInfra.getProperties.mockReturnValueOnce([
+        { id: 'test_property1_id', name: '屬性1' },
+      ])
+      mockDefinitionInfra.getTags.mockReturnValueOnce([
+        { name: 'test_tag1' },
+      ]);
+
+      const [member] = await service.rawCsvToMember('test-app-id', rawRows);
+      expect(member.id).toBe(memberId);
+      expect(member.name).toBe('test_extra_unknown');
+      expect(member.username).toBe(memberId);
+      expect(member.email).toBe('test_extra_unknown_email@test.com');
+      expect(member.memberPhones.length).toBe(1);
+      expect(member.memberPhones[0].phone).toEqual('0912345678');
+      expect(member.memberCategories.length).toBe(1);
+      expect(member.memberCategories[0].category.id).toEqual('test_category1_id');
+      expect(member.memberProperties.length).toBe(1);
+      expect(member.memberProperties[0].property.id).toEqual('test_property1_id');
+      expect(member.memberProperties[0].value).toEqual('test_property1');
+      expect(member.memberTags.length).toBe(1);
+      expect(member.memberTags[0].tagName2.name).toEqual('test_tag1');
+      expect(member.star).toBe(999);
+      expect(member.createdAt).toBe(createdAt);
+    });
   });
 });
