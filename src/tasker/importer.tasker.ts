e8ee12f2de61e9f7c5bdf9bce298c7af71deeb78
Clooooode <jackey8616@gmail.com>
2023-06-02 12:45:35 +0800

    fix: apply to v3 aws-sdk

--- src/tasker/importer.tasker.ts
+++ src/tasker/importer.tasker.ts
@@ -74,8 +74,10 @@ export class ImporterTasker extends Tasker {
         if (`"${checksumETag}"` !== ETag) {
           throw new Error('Unmatched checksum.');
         }
+        const uint8Array = await Body.transformToByteArray();
+
         const insertResult = await this.importToDatabase(
-          appId, category, ContentType, Body as unknown as Buffer,
+          appId, category, ContentType, Buffer.from(uint8Array),
         );
         this.putEmailQueue(
           appId,
