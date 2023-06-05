be7753fe9a6c9745c4592d8bc5de067d248c7f04
Clooooode <jackey8616@gmail.com>
2023-06-05 19:26:32 +0800

    chore: remove aws credentials env

--- src/utility/storage/storage.service.ts
+++ src/utility/storage/storage.service.ts
@@ -17,32 +17,21 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class StorageService {
-  private readonly awsS3AccessKeyId: string;
-  private readonly awsS3SecretAccessKey: string;
   private readonly awsS3BucketStatic: string;
   private readonly awsS3BucketStorage: string;
 
   constructor(
     private readonly configService: ConfigService<{
-      AWS_S3_ACCESS_KEY_ID: string;
-      AWS_S3_SECRET_ACCESS_KEY: string;
       AWS_S3_BUCKET_STATIC: string;
       AWS_S3_BUCKET_STORAGE: string;
     }>,
   ) {
-    this.awsS3AccessKeyId = configService.getOrThrow('AWS_S3_ACCESS_KEY_ID');
-    this.awsS3SecretAccessKey = configService.getOrThrow('AWS_S3_SECRET_ACCESS_KEY');
     this.awsS3BucketStatic = configService.getOrThrow('AWS_S3_BUCKET_STATIC');
     this.awsS3BucketStorage = configService.getOrThrow('AWS_S3_BUCKET_STORAGE');
   }
 
   private s3(): S3Client {
-    return new S3Client({
-      credentials: {
-        accessKeyId: this.awsS3AccessKeyId,
-        secretAccessKey: this.awsS3SecretAccessKey,
-      },
-    });
+    return new S3Client({});
   }
 
   getSignedUrlForDownloadStorage(key: string, expiresIn: number): Promise<string> {
