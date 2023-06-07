e52a916306ee2ab80698808c6141151958d879ca
Clooooode <jackey8616@gmail.com>
2023-06-07 11:38:48 +0800

    feat: apply ValidatePipe to DTOs

--- src/member/member.dto.ts
+++ src/member/member.dto.ts
@@ -1,14 +1,33 @@
+import { Type } from 'class-transformer';
+import { IsArray, IsOptional, IsString, ValidateNested } from 'class-validator';
+
+class FileInfo {
+  @IsString()
+  key: string;
+
+  @IsString()
+  checksum: string;
+}
+
 export class MemberImportDTO {
+  @IsString()
   appId: string;
-  fileInfos: Array<{
-    key: string;
-    checksum: string;
-  }>;
+
+  @ValidateNested()
+  @Type(() => FileInfo)
+  fileInfos: Array<FileInfo>;
 }
 
 export class MemberExportDTO {
+  @IsString()
   appId: string;
+
+  @IsArray()
+  @IsString({ each: true })
   memberIds: Array<string>;
+
+  @IsString()
+  @IsOptional()
   exportMime?: string;
 }
 
