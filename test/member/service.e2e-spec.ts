23ee06aaff603203e25334baadee8a011166d5d5
Clooooode <jackey8616@gmail.com>
2023-06-26 12:06:15 +0800

    test: e2e cases for MemberService

--- test/member/service.e2e-spec.ts
+++ test/member/service.e2e-spec.ts
@@ -1,5 +1,6 @@
 import { v4 } from 'uuid';
 import { EntityManager, Repository } from 'typeorm';
+import { ValidationError } from 'class-validator';
 import { INestApplication } from '@nestjs/common';
 import { getEntityManagerToken } from '@nestjs/typeorm';
 import { Test } from '@nestjs/testing';
@@ -452,6 +453,351 @@ describe('MemberService (e2e)', () => {
       expect(member.memberTags.length).toBe(1);
       expect(member.memberTags.find(({ tagName2 }) => tagName2.name === memberTag.name)).toBeUndefined();
       expect(member.memberTags.find(({ tagName2 }) => tagName2.name === anotherMemberTag.name)).not.toBeUndefined();
+    });
+
+    describe('Should raise error if some fields missed when import for insert new data', () => {
+      async function testField(
+        testFieldName: string,
+        testDataPostfix: string,
+        toSet: Function,
+        toExpect: Function,
+        fullMatch = true,
+      ) {
+        const rawRows = [
+          {
+            '流水號': 'id',
+            '姓名': 'name',
+            '帳號': 'username',
+            '信箱': 'email',
+            '身份': 'role',
+            '星等': 'star',
+            '建立日期': 'createdAt',
+            '上次登入日期': 'loginedAt',
+            '手機1': 'phones.0',
+            '分類1': 'categories.0',
+            [memberProperty.name]: 'properties.0',
+            '標籤1': 'tags.0',
+          },
+          {
+            '流水號': '',
+            '姓名': 'to-insert-name',
+            '帳號': 'to-insert-username',
+            '信箱': 'to-insert-email@example.com',
+            '身份': 'to-insert-role',
+            '星等': '999',
+            '建立日期': '',
+            '上次登入日期': '',
+            '手機1': '',
+            '分類1': '',
+            [memberProperty.name]: '',
+            '標籤1': '',
+          },
+        ];
+
+        const readableHeaderNames = Object
+          .entries(rawRows[0] as Record<string, string>)
+          .filter(([_, codeReadable]) => fullMatch
+            ? codeReadable === testFieldName
+            : codeReadable.includes(testFieldName))
+          .map(([humanReadable]) => humanReadable);
+        for (let i = 0; i < rawRows.length; i++) {
+          for (let j = 0; j < readableHeaderNames.length; j++) {
+            toSet(readableHeaderNames[j], rawRows[i]);
+          }
+        }
+
+        const result = await service.processImportFromFile(app.id, rawRows);
+        expect(result.toInsertCount).toBe(1);
+        expect(result.insertedCount).toBe(0);
+        expect(result.failedCount).toBe(1);
+        expect(result.failedErrors.length).toBe(1);
+        toExpect(result);
+      };
+      const setUndefined = (key: string, value: Record<string, string>) =>
+        delete value[key];
+      const setBlank = (key: string, value: Record<string, string>) =>
+        value[key] = '';
+
+      it('Field username', async () => {
+        const toExpect = ({ failedErrors }: {
+          failedErrors: Array<Record<string, Array<ValidationError>>>;
+        }) => {
+          const error = Object.values(failedErrors.pop()).pop()
+            .find(({ property }) => property === 'username');
+          expect(error.constraints.isNotEmpty).not.toBeUndefined();
+          expect(error.constraints.isString).not.toBeUndefined();
+        };
+        await testField('username', 'blank', setBlank, toExpect),
+        await testField('username', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field star', async () => {
+        const toExpect = ({ failedErrors }: {
+          failedErrors: Array<Record<string, Array<ValidationError>>>;
+        }) => {
+          const error = Object.values(failedErrors.pop()).pop()
+            .find(({ property }) => property === 'star');
+          expect(error.constraints.isNotEmpty).not.toBeUndefined();
+          expect(error.constraints.isNumberString).not.toBeUndefined();
+        };
+        await testField('star', 'blank', setBlank, toExpect),
+        await testField('star', 'undefined', setUndefined, toExpect);
+      });
+    });
+
+    describe('Should remained value if given field is blank or undefined', () => {
+      async function testField(
+        testFieldName: string,
+        testDataPostfix: string,
+        toSet: Function,
+        toExpect: Function,
+        fullMatch = true,
+      ) {
+        let insertedMember = new Member();
+        insertedMember.appId = app.id;
+        insertedMember.id = v4();
+        insertedMember.name = `${testFieldName}_${testDataPostfix}`;
+        insertedMember.username = `acc_${testFieldName}_${testDataPostfix}`;
+        insertedMember.email = `mail_${testFieldName}_${testDataPostfix}@example.com`;
+        insertedMember.role = 'general-member';
+        insertedMember.star = 0;
+        insertedMember.createdAt = new Date();
+        insertedMember.loginedAt = new Date();
+
+        let insertedMemberPhone = new MemberPhone();
+        insertedMemberPhone.member = insertedMember;
+        insertedMemberPhone.phone = '0987654321';
+
+        let insertedMemberCategory = new MemberCategory();
+        insertedMemberCategory.member = insertedMember;
+        insertedMemberCategory.category = category;
+        insertedMemberCategory.position = 0;
+
+        let insertedMemberProperty = new MemberProperty();
+        insertedMemberProperty.member = insertedMember;
+        insertedMemberProperty.property = memberProperty;
+        insertedMemberProperty.value = '1234';
+
+        let insertedMemberTag = new MemberTag();
+        insertedMemberTag.member = insertedMember;
+        insertedMemberTag.tagName2 = memberTag;
+
+        insertedMember = await memberRepo.save(insertedMember);
+        insertedMemberPhone = await memberPhoneRepo.save(insertedMemberPhone);
+        insertedMemberCategory = await memberCategoryRepo.save(insertedMemberCategory);
+        insertedMemberProperty = await memberPropertyRepo.save(insertedMemberProperty);
+        insertedMemberTag = await memberTagRepo.save(insertedMemberTag);
+
+        const rawRows = [
+          {
+            '流水號': 'id',
+            '姓名': 'name',
+            '帳號': 'username',
+            '信箱': 'email',
+            '身份': 'role',
+            '星等': 'star',
+            '建立日期': 'createdAt',
+            '上次登入日期': 'loginedAt',
+            '手機1': 'phones.0',
+            '分類1': 'categories.0',
+            [insertedMemberProperty.property.name]: 'properties.0',
+            '標籤1': 'tags.0',
+          },
+          {
+            '流水號': insertedMember.id,
+            '姓名': insertedMember.name,
+            '帳號': insertedMember.username,
+            '信箱': insertedMember.email,
+            '身份': insertedMember.role,
+            '星等': insertedMember.star,
+            '建立日期': insertedMember.createdAt.toISOString(),
+            '上次登入日期': insertedMember.loginedAt.toISOString(),
+            '手機1': insertedMemberPhone.phone,
+            '分類1': insertedMemberCategory.category.name,
+            [insertedMemberProperty.property.name]: insertedMemberProperty.value,
+            '標籤1': insertedMemberTag.tagName2.name,
+          },
+        ];
+
+        const readableHeaderNames = Object
+          .entries(rawRows[0] as Record<string, string>)
+          .filter(([_, codeReadable]) => fullMatch
+            ? codeReadable === testFieldName
+            : codeReadable.includes(testFieldName))
+          .map(([humanReadable]) => humanReadable);
+        for (let i = 0; i < rawRows.length; i++) {
+          for (let j = 0; j < readableHeaderNames.length; j++) {
+            toSet(readableHeaderNames[j], rawRows[i]);
+          }
+        }
+
+        const result = await service.processImportFromFile(app.id, rawRows);
+        expect(result.toInsertCount).toBe(1);
+        expect(result.insertedCount).toBe(1);
+        expect(result.failedCount).toBe(0);
+        expect(result.failedErrors.length).toBe(0);
+        const member = await memberRepo.findOne({
+          where: { id: insertedMember.id },
+          relations: {
+            memberCategories: { category: true },
+            memberProperties: { property: true },
+            memberPhones: true,
+            memberTags: { tagName2: true },
+          },
+        });
+        expect(member.id).toBe(insertedMember.id);
+        expect(member.email).toBe(insertedMember.email);
+        toExpect({
+          member,
+          insertedMember,
+          insertedMemberPhone,
+          insertedMemberCategory,
+          insertedMemberProperty,
+          insertedMemberTag,
+        });
+      };
+      const setUndefined = (key: string, value: Record<string, string>) =>
+        delete value[key];
+      const setBlank = (key: string, value: Record<string, string>) =>
+        value[key] = '';
+
+      it('Field name', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect(member.name).toBe(insertedMember.name);
+        };
+        await testField('name', 'blank', setBlank, toExpect);
+        await testField('name', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field username', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect([insertedMember.id, insertedMember.username]).toContain(member.username);
+        };
+        await testField('username', 'blank', setBlank, toExpect),
+        await testField('username', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field role', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect(member.role).toBe(insertedMember.role);
+        };
+        await testField('role', 'blank', setBlank, toExpect),
+        await testField('role', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field star', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect(member.star).toBe(insertedMember.star);
+        };
+        await testField('star', 'blank', setBlank, toExpect),
+        await testField('star', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field createdAt', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect(member.createdAt).toStrictEqual(insertedMember.createdAt);
+        };
+        await testField('createdAt', 'blank', setBlank, toExpect),
+        await testField('createdAt', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field loginedAt', async () => {
+        const toExpect = ({ member, insertedMember }: { member: Member; insertedMember: Member }) => {
+          expect(member.loginedAt).toStrictEqual(insertedMember.loginedAt);
+        };
+        await testField('loginedAt', 'blank', setBlank, toExpect),
+        await testField('loginedAt', 'undefined', setUndefined, toExpect);
+      });
+
+      it('Field phones', async () => {
+        const toExpect = ({
+          member,
+          insertedMemberCategory,
+          insertedMemberProperty,
+          insertedMemberTag,
+        }: {
+          member: Member;
+          insertedMemberCategory: MemberCategory;
+          insertedMemberProperty: MemberProperty;
+          insertedMemberTag: MemberTag;
+        }) => {
+          expect(member.memberPhones.length).toBe(0);
+          expect(member.memberCategories[0].category.id).toBe(insertedMemberCategory.category.id);
+          expect(member.memberProperties[0].property.name).toBe(insertedMemberProperty.property.name);
+          expect(member.memberProperties[0].value).toBe(insertedMemberProperty.value);
+          expect(member.memberTags[0].tagName2.name).toBe(insertedMemberTag.tagName2.name);
+        };
+        await testField('phones', 'blank', setBlank, toExpect, false),
+        await testField('phones', 'undefined', setUndefined, toExpect, false);
+      });
+
+      it('Field categories', async () => {
+        const toExpect = ({
+          member,
+          insertedMemberPhone,
+          insertedMemberProperty,
+          insertedMemberTag,
+        }: {
+          member: Member;
+          insertedMemberPhone: MemberPhone;
+          insertedMemberProperty: MemberProperty;
+          insertedMemberTag: MemberTag;
+        }) => {
+          expect(member.memberPhones[0].phone).toBe(insertedMemberPhone.phone);
+          expect(member.memberCategories.length).toBe(0);
+          expect(member.memberProperties[0].property.name).toBe(insertedMemberProperty.property.name);
+          expect(member.memberProperties[0].value).toBe(insertedMemberProperty.value);
+          expect(member.memberTags[0].tagName2.name).toBe(insertedMemberTag.tagName2.name);
+        };
+        await testField('categories', 'blank', setBlank, toExpect, false),
+        await testField('categories', 'undefined', setUndefined, toExpect, false);
+      });
+
+      it('Field properties', async () => {
+        const toExpect = ({
+          member,
+          insertedMemberPhone,
+          insertedMemberCategory,
+          insertedMemberProperty,
+          insertedMemberTag,
+        }: {
+          member: Member;
+          insertedMemberPhone: MemberPhone;
+          insertedMemberCategory: MemberCategory;
+          insertedMemberProperty: MemberProperty;
+          insertedMemberTag: MemberTag;
+        }) => {
+          expect(member.memberPhones[0].phone).toBe(insertedMemberPhone.phone);
+          expect(member.memberCategories[0].category.id).toBe(insertedMemberCategory.category.id);
+          expect(member.memberProperties[0].property.name).toBe(insertedMemberProperty.property.name);
+          expect(member.memberProperties[0].value).toBe(insertedMemberProperty.value);
+          expect(member.memberTags[0].tagName2.name).toBe(insertedMemberTag.tagName2.name);
+        };
+        await testField('properties', 'blank', setBlank, toExpect, false),
+        await testField('properties', 'undefined', setUndefined, toExpect, false);
+      });
+
+      it('Field tags', async () => {
+        const toExpect = ({
+          member,
+          insertedMemberPhone,
+          insertedMemberCategory,
+          insertedMemberProperty,
+        }: {
+          member: Member;
+          insertedMemberPhone: MemberPhone;
+          insertedMemberCategory: MemberCategory;
+          insertedMemberProperty: MemberProperty;
+        }) => {
+          expect(member.memberPhones[0].phone).toBe(insertedMemberPhone.phone);
+          expect(member.memberCategories[0].category.id).toBe(insertedMemberCategory.category.id);
+          expect(member.memberProperties[0].property.name).toBe(insertedMemberProperty.property.name);
+          expect(member.memberProperties[0].value).toBe(insertedMemberProperty.value);
+          expect(member.memberTags.length).toBe(0);
+        };
+        await testField('tags', 'blank', setBlank, toExpect, false),
+        await testField('tags', 'undefined', setUndefined, toExpect, false);
+      });
     });
   });
 });
