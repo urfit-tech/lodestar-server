85368455314d238549ddba10a13d5b14ad15f01d
Clooooode <jackey8616@gmail.com>
2023-06-27 23:43:50 +0800

    feat: apply print memory usage @ Tasker & Runner

--- src/tasker/tasker.ts
+++ src/tasker/tasker.ts
@@ -1,1 +1,20 @@
-export class Tasker {}
+import { Logger } from '@nestjs/common';
+
+import { getMemoryUsageString } from '~/utils';
+
+export class Tasker {
+  protected readonly logger: Logger;
+
+  constructor(logger: Logger) {
+    this.logger = logger;
+  }
+
+  protected preProcess(): void {
+    this.logger.log(getMemoryUsageString());
+  }
+
+  protected postProcess(): void {
+    this.logger.log('Task execution finished');
+    this.logger.log(getMemoryUsageString());
+  }
+}
