00ffcbe8f281e6b7115710b3d80733d5ac2aa72b
Clooooode <jackey8616@gmail.com>
2023-08-11 13:23:07 +0800

    feat: api endpoint for get members

--- src/member/member.controller.ts
+++ src/member/member.controller.ts
@@ -1,13 +1,22 @@
 import { Queue } from 'bull';
 import jwt from 'jsonwebtoken';
-import { Body, Controller, Headers, Logger, Post, UnauthorizedException } from '@nestjs/common';
+import { 
+  Logger,
+  Controller,
+  Headers,
+  Body,
+  Get,
+  Post,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectQueue } from '@nestjs/bull';
 import { ConfigService } from '@nestjs/config';
 
 import { ImportJob, ImporterTasker } from '~/tasker/importer.tasker';
 import { ExporterTasker, MemberExportJob } from '~/tasker/exporter.tasker';
 
-import { MemberExportDTO, MemberImportDTO } from './member.dto';
+import { MemberExportDTO, MemberGetConditionDTO, MemberGetResultDTO, MemberImportDTO } from './member.dto';
+import { MemberService } from './member.service';
 
 @Controller({
   path: 'members',
@@ -23,6 +32,7 @@ export class MemberController {
     private readonly configService: ConfigService<{
       HASURA_JWT_SECRET: string;
     }>,
+    private readonly memberService: MemberService,
   ) {
     this.jwtSecret = configService.getOrThrow('HASURA_JWT_SECRET');
   }
@@ -35,6 +45,42 @@ export class MemberController {
       this.logger.error(error);
       throw new UnauthorizedException();
     }
+  }
+
+  @Get()
+  public async getMembers(
+    @Headers('Authorization') authorization: string,
+    @Body() queryCondition: MemberGetConditionDTO,
+  ): Promise<Array<MemberGetResultDTO>> {
+    const { appId, permissions } = this.verify(authorization);
+
+    if (![
+      'MEMBER_ADMIN',
+      'POST_ADMIN',
+      'SALES_RECORDS_NORMAL',
+      'SALES_RECORDS_ADMIN',
+      'PROGRAM_ADMIN',
+      'PROGRAM_PACKAGE_TEMPO_DELIVERY_ADMIN',
+      'APPOINTMENT_PLAN_ADMIN',
+      'COIN_ADMIN',
+      'SALES_LEAD_SELECTOR_ADMIN',
+      'SHIPPING_ADMIN',
+      'SHIPPING_NORMAL',
+      'MEMBER_PHONE_ADMIN',
+      'PROJECT_PORTFOLIO_NORMAL',
+      'PROJECT_PORTFOLIO_ADMIN',
+      'SALES_PERFORMANCE_ADMIN',
+      'SALES_LEAD_ADMIN',
+      'SALES_LEAD_NORMAL',
+      'MATERIAL_AUDIT_LOG_ADMIN',
+    ].some((e) => permissions.includes(e))) {
+      throw new UnauthorizedException(
+        { message: 'missing required permission' },
+        'User permission is not met required permissions.',
+      );
+    }
+
+    return this.memberService.getMembersByCondition(appId, queryCondition);
   }
 
   @Post('import')
