aaf1320bb8957a98cf59e21f8c9d3880c5e2424e
Clooooode <jackey8616@gmail.com>
2023-08-18 12:26:38 +0800

    fix: incorrect verfiy usage

--- src/order/order.controller.ts
+++ src/order/order.controller.ts
@@ -1,4 +1,6 @@
+import { verify } from 'jsonwebtoken';
 import { Body, Controller, Get, Headers, Param, Put } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { AuthService } from '~/auth/auth.service';
 import { APIException } from '~/api.excetion';
@@ -11,7 +13,11 @@ import { TransferReceivedOrderBodyDTO, TransferReceivedOrderDTO, TransferReceive
   version: ['2'],
 })
 export class OrderController {
-  constructor(private authService: AuthService, private orderService: OrderService) {}
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService,
+    private readonly configService: ConfigService<{ HASURA_JWT_SECRET: string }>,
+  ) {}
 
   @Put('transfer-received-order')
   async transferOrder(@Body() dto: TransferReceivedOrderBodyDTO, @Headers() headers) {
@@ -26,7 +32,7 @@ export class OrderController {
     }
 
     try {
-      transferOrderToken = this.authService.verify(token) as TransferReceivedOrderToken;
+      transferOrderToken = verify(token, this.configService.get('HASURA_JWT_SECRET')) as TransferReceivedOrderToken;
     } catch {
       throw new APIException({ code: 'E_TOKEN_INVALID', message: 'transferOrderToken is invalid' });
     }
