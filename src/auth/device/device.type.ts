d56a3f3cc5ea802d0ac486451fb732946453db42
Clooooode <jackey8616@gmail.com>
2023-09-20 12:18:58 +0800

    fix: add missing device.type for testing

---
+++ src/auth/device/device.type.ts
@@ -0,0 +1,8 @@
+export enum LoginDeviceStatus {
+  EXISTED = 'existed',
+  AVAILABLE = 'available',
+  LIMITED = 'limited',
+  UNSUPPORTED = 'unsupported',
+  BIND_LIMIT_EXCEED = 'bindLimitExceeded',
+  LOGIN_LIMIT_EXCEED = 'loginLimitExceeded',
+};
