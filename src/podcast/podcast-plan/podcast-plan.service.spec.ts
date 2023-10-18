09e615c725a38c45d246df931fb64fd4f3415575
Logan <clothe09986@gmail.com>
2023-10-19 02:56:49 +0800

    fix: add missing provide in podcast-plan spec

--- src/podcast/podcast-plan/podcast-plan.service.spec.ts
+++ src/podcast/podcast-plan/podcast-plan.service.spec.ts
@@ -1,4 +1,10 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { EntityManager } from 'typeorm';
+import { DefinitionInfrastructure } from '~/definition/definition.infra';
+import { MemberInfrastructure } from '~/member/member.infra';
+import { MemberService } from '~/member/member.service';
+import { UtilityService } from '~/utility/utility.service';
+import { PodcastPlanInfrastructure } from './podcast-plan.infra';
 import { PodcastPlanService } from './podcast-plan.service';
 
 describe('PodcastPlanService', () => {
@@ -6,7 +12,15 @@ describe('PodcastPlanService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [PodcastPlanService],
+      providers: [
+        PodcastPlanService,
+        PodcastPlanInfrastructure,
+        MemberService,
+        EntityManager,
+        UtilityService,
+        DefinitionInfrastructure,
+        MemberInfrastructure,
+      ],
     }).compile();
 
     service = module.get<PodcastPlanService>(PodcastPlanService);
