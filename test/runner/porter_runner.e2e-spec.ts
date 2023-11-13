a4b8892c265d5d268fb2e7a893aa7bd364661e4c
Ted <wodf410@gmail.com>
2023-11-13 23:54:41 +0800

    test: add more description and add member instrance

--- test/runner/porter_runner.e2e-spec.ts
+++ test/runner/porter_runner.e2e-spec.ts
@@ -8,6 +8,14 @@ import { EntityManager, Repository } from 'typeorm';
 import { getEntityManagerToken } from '@nestjs/typeorm';
 import { Member } from '~/member/entity/member.entity';
 import { Runner } from '~/runner/runner';
+import { v4 } from 'uuid';
+import { Role } from '~/entity/Role';
+import { App } from '~/app/entity/app.entity';
+import { app, appHost, appPlan, appSecret, appSetting, member, role } from '../data';
+import { AppPlan } from '~/entity/AppPlan';
+import { AppHost } from '~/app/entity/app_host.entity';
+import { AppSecret } from '~/app/entity/app_secret.entity';
+import { AppSetting } from '~/app/entity/app_setting.entity';
 
 jest.mock('axios', () => ({
   get: jest.fn()
@@ -15,19 +23,15 @@ jest.mock('axios', () => ({
 
 describe('PorterRunner', () => {
   let application: INestApplication;
-  
   let cacheService: any;
   let manager: EntityManager;
+  let roleRepo: Repository<Role>;
+  let appPlanRepo: Repository<AppPlan>;
+  let appRepo: Repository<App>;
+  let appHostRepo: Repository<AppHost>;
+  let appSecretRepo: Repository<AppSecret>;
+  let appSettingRepo: Repository<AppSetting>;
   let memberRepo: Repository<Member>;
-
-  const mockCacheService = {
-    getClient: jest.fn().mockReturnValue({
-      get: jest.fn().mockResolvedValue(['last-logged-in:123']),
-      mget: jest.fn().mockResolvedValue(['2023-01-01T00:00:00Z']),
-      del: jest.fn().mockResolvedValue(null),
-    }),
-  };
-
 
   beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -40,7 +44,6 @@ describe('PorterRunner', () => {
         }),
       ],
       providers: [
-        { provide: CacheService, useValue: mockCacheService },
       ],
     }).compile();
 
@@ -48,45 +51,74 @@ describe('PorterRunner', () => {
 
     cacheService = application.get<CacheService>(CacheService);
     manager = application.get<EntityManager>(getEntityManagerToken());
+
+    appPlanRepo = manager.getRepository(AppPlan);
+    appRepo = manager.getRepository(App);
+    appSettingRepo = manager.getRepository(AppSetting);
+    appSecretRepo = manager.getRepository(AppSecret);
+    appHostRepo = manager.getRepository(AppHost);
+    roleRepo = manager.getRepository(Role);
     memberRepo = manager.getRepository(Member);
-    
 
     await memberRepo.delete({});
-    
+    await appSettingRepo.delete({});
+    await appSecretRepo.delete({});
+    await appHostRepo.delete({});
+    await appRepo.delete({});
+    await appPlanRepo.delete({});
+    await roleRepo.delete({});
+
+
     await application.init();
+  });
+
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
 
   afterAll(async () => {
     await application.close();
   });
   
-
-
-  it('should call the heartbeat URL if PORTER_HEARTBEAT_URL is set', async () => {
-    const mockedAxiosGet = axios.get as jest.Mock;
-    const testUrl = 'http://test-heartbeat-url.com';
-    process.env.PORTER_HEARTBEAT_URL = testUrl;
-
-    mockedAxiosGet.mockResolvedValue({}); 
-
-    const porterRunner = application.get<PorterRunner>(Runner);
-    await porterRunner.execute();
-
-    expect(mockedAxiosGet).toHaveBeenCalledWith(testUrl);
-  });
-
-  it('should get the redis key last logged in', async () => {
-    jest.spyOn(cacheService, 'getClient').mockReturnValue({
-      get: jest.fn().mockResolvedValue(['last-logged-in:123']),
-      mget: jest.fn().mockResolvedValue(['2023-01-01T00:00:00Z']),
-      del: jest.fn().mockResolvedValue(null),
-    });
-
-    const porterRunner = application.get<PorterRunner>(Runner);
-    await porterRunner.execute();
+  describe("testing heartbeat",  () => {
+    it('should call the heartbeat URL if PORTER_HEARTBEAT_URL is set', async () => {
+      const mockedAxiosGet = axios.get as jest.Mock;
+      const testUrl = 'http://test-heartbeat-url.com';
+      process.env.PORTER_HEARTBEAT_URL = testUrl;
   
-    expect(cacheService.getClient().get).toHaveBeenCalledWith('last-logged-in:*');
-  });
+      const porterRunner = application.get<PorterRunner>(Runner);
+      await porterRunner.execute();
+  
+      expect(mockedAxiosGet).toHaveBeenCalledWith(testUrl);
+    });
+  })
+
+  describe("last-logged-in", () => {
+    describe('success process', () => {
+      beforeAll(async ()=> {
+        jest.spyOn(cacheService, 'getClient').mockReturnValue({
+          get: jest.fn().mockResolvedValue(['last-logged-in:123']),
+          mget: jest.fn().mockResolvedValue(['2023-01-01T00:00:00Z']),
+          del: jest.fn().mockResolvedValue(null),
+        });
+        await roleRepo.save(role);
+        await appPlanRepo.save(appPlan);
+        await appRepo.save(app);
+        await appSettingRepo.save(appSetting);
+        await appSecretRepo.save(appSecret);
+        await appHostRepo.save(appHost);
+        await memberRepo.save(member);
+      })
+
+      it('should get the redis key last logged in', async () => {
+        
+        const porterRunner = application.get<PorterRunner>(Runner);
+        await porterRunner.execute();
+      
+        expect(cacheService.getClient().get).toHaveBeenCalledWith('last-logged-in:*');
+      });
 
 
+    })
+  })
 });
