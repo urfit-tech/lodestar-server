7be6aa045f5d77a09cd0787e9b6ec037bc4fd4b8
Ted <wodf410@gmail.com>
2023-11-13 09:11:28 +0800

    feat: add portLastLoggedIn

--- src/runner/porter.runner.ts
+++ src/runner/porter.runner.ts
@@ -31,10 +31,43 @@ export class PorterRunner extends Runner {
     super(PorterRunner.name, 1000, logger, distributedLockService, shutdownService);
   }
 
+  async portLastLoggedIn(): Promise<void> {
+    console.log("portLastLoggedIn")
 
+    const lastLoggedInMemberKeys: string | null = await this.cacheService.getClient().get('last-logged-in:*');
+    
+    const memberLastLoggedInTimestamps: (string | null)[] = lastLoggedInMemberKeys.length > 0 
+        ? await this.cacheService.getClient().mget(lastLoggedInMemberKeys) 
+        : [];
+
+    for (let index = 0; index < lastLoggedInMemberKeys.length; index++) {
+      const key: string = lastLoggedInMemberKeys[index];
+      const [, memberId]: string[] = key.split(':');
+      
+      const loginedAt: string | null = memberLastLoggedInTimestamps[index];
+
+      try {
+        await this.entityManager.update(Member, memberId, { loginedAt });
+        await this.cacheService.getClient().del(key);
+      } catch (error) {
+        console.error(`porting ${key} failed:`, error);
+      }
+    }
+  }
 
   async execute(): Promise<void> {
     console.log("start")
+    if (process.env.PORTER_HEARTBEAT_URL) {
+      await axios.get(process.env.PORTER_HEARTBEAT_URL)
+    }
+
+    try {
+      console.log('porting last logged in')
+      await this.portLastLoggedIn()
+      console.log('finishing porting last logged in')
+    } catch (error) {
+      console.error('port last logged in failed:', error)
+    }
   }
 
 }
