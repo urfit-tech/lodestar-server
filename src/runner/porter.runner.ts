a9c1817b708b791569a7967f928c49d66e54c526
Ted <wodf410@gmail.com>
2023-11-15 08:58:42 +0800

    feat: use scan instead of keys

--- src/runner/porter.runner.ts
+++ src/runner/porter.runner.ts
@@ -30,11 +30,17 @@ export class PorterRunner extends Runner {
   ) {
     super(PorterRunner.name, 1000, logger, distributedLockService, shutdownService);
   }
+  
 
   async portLastLoggedIn(): Promise<void> {
     console.log("portLastLoggedIn")
 
-    const lastLoggedInMemberKeys: string | null = await this.cacheService.getClient().get('last-logged-in:*');
+    const lastLoggedInMemberKeys: string[] | null = await this.cacheService.getClient().keys('last-logged-in:*');
+
+    if (!lastLoggedInMemberKeys) {
+      console.warn('No last logged in member keys found');
+      return;
+    }
     
     const memberLastLoggedInTimestamps: (string | null)[] = lastLoggedInMemberKeys.length > 0 
         ? await this.cacheService.getClient().mget(lastLoggedInMemberKeys) 
@@ -44,29 +50,45 @@ export class PorterRunner extends Runner {
       const key: string = lastLoggedInMemberKeys[index];
       const [, memberId]: string[] = key.split(':');
       
-      const loginedAt: string | null = memberLastLoggedInTimestamps[index];
-
+      let loginedAt: string | null = memberLastLoggedInTimestamps[index];
+    
+      if (loginedAt) {
+        const localDate = new Date(loginedAt);
+        loginedAt = localDate.toISOString();
+      }
+    
       try {
         await this.entityManager.update(Member, memberId, { loginedAt });
+    
+        await this.entityManager.findOne(Member, { where: { id: memberId } });   
+    
         await this.cacheService.getClient().del(key);
       } catch (error) {
         console.error(`porting ${key} failed:`, error);
       }
     }
+        
   }
 
   async portPlayerEvent(): Promise<void> {
-    const keys: string[] = await this.cacheService.getClient().keys('program-content-event:*:program-content:*:*');
+    let cursor = '0';
+    let scanResult: [string, string[]];
+    const pattern = 'program-content-event:*:program-content:*:*';
+    const count = 10000;
+    this.cacheService.getClient().scanStream()
+  
+    do {
+      scanResult = await this.cacheService.getClient().scan(cursor, 'MATCH', pattern, 'COUNT', count);
+      cursor = scanResult[0];
+      const keys = scanResult[1];
+      const programContentLogs: ProgramContentLog[] = [];
 
-    const values: (string | null)[] = keys.length > 0 ? await this.cacheService.getClient().mget(keys) : [];
-
-    for (let index = 0; index < keys.length; index++) {
-        const key: string = keys[index];
-      const [, memberId, , programContentId]: string[] = key.split(':');
-
-      const value: { playbackRate?: number; startedAt?: number; endedAt?: number } = JSON.parse(values[index] || '{}');
-
-      try {
+      for (let key of keys) {
+        const [, memberId, , programContentId]: string[] = key.split(':');
+        const valueString = await this.cacheService.getClient().get(key);
+        const value: { playbackRate?: number; startedAt?: number; endedAt?: number } = JSON.parse(valueString || '{}');
+  
+        try {
           const programContent: ProgramContent = await this.programService.getProgramContentById(programContentId); 
           const programContentLog = new ProgramContentLog();
           programContentLog.memberId = memberId;
@@ -74,41 +96,65 @@ export class PorterRunner extends Runner {
           programContentLog.playbackRate = value.playbackRate;
           programContentLog.startedAt = value.startedAt || 0;
           programContentLog.endedAt = value.endedAt || 0;
-
-          await this.entityManager.save(programContentLog);
+  
+          programContentLogs.push(programContentLog); 
           await this.cacheService.getClient().del(key);
-      } catch (error) {
-          console.error(`porting ${key} failed: ${error}`)
+        } catch (error) {
+          console.error(`Processing ${key} failed: ${error}`);
+        }
       }
-    }
+  
+      if (programContentLogs.length > 0) {
+        try {
+          await this.entityManager.save(programContentLogs);
+        } catch (error) {
+          console.error(`Batch saving failed: ${error}`);
+        }
+      }
+    } while (cursor !== '0');
   }
-
+  
   async portPodcastProgram(): Promise<void> {
-    const keys: string[] = await this.cacheService.getClient().keys('podcast-program-event:*:podcast-program:*:*');
-
-    const values: (string | null)[] = keys.length > 0 ? await this.cacheService.getClient().mget(keys) : [];
-
-    for (let index = 0; index < keys.length; index++) {
-      const key: string = keys[index];
-      const [, memberId, , podcastProgramId]: string[] = key.split(':');
-
-      const value: { progress?: number; podcastAlbumId?: string } = JSON.parse(values[index] || '{}');
-
-      try {
-        await this.podcastService.upsertPodcastProgramProgress(
-            memberId,
-            podcastProgramId,
-            value.progress,
-            value.progress,
-            value.podcastAlbumId || null,
-        );
-
-        await this.cacheService.getClient().del(key);
-      } catch (error) {
-        console.error(`porting ${key} failed: ${error}`);
+    let cursor = '0';
+    let scanResult;
+    const pattern = 'podcast-program-event:*:podcast-program:*:*';
+    const count = 10000; 
+  
+    do {
+      scanResult = await this.cacheService.getClient().scan(cursor, 'MATCH', pattern, 'COUNT', count);
+      cursor = scanResult[0];
+      const keys = scanResult[1];
+  
+      if (keys.length > 0) {
+        const values: (string | null)[] = await this.cacheService.getClient().mget(keys);
+        const updateData = []; 
+  
+        for (let index = 0; index < keys.length; index++) {
+          const key: string = keys[index];
+          const [, memberId, , podcastProgramId]: string[] = key.split(':');
+          const value: { progress?: number; podcastAlbumId?: string } = JSON.parse(values[index] || '{}');
+  
+          updateData.push({ 
+            memberId: memberId,
+            podcastProgramId: podcastProgramId,
+            progress: value.progress,
+            lastProgress: value.progress,
+            podcastAlbumId: value.podcastAlbumId || null
+          });
+        }
+  
+        try {
+          if (updateData.length > 0) {
+            await this.podcastService.upsertPodcastProgramProgressBatch(updateData);
+            await Promise.all(keys.map(key => this.cacheService.getClient().del(key))); 
+          }
+        } catch (error) {
+          console.error(`Batch upsert failed: ${error}`);
+        }
       }
-    }
+    } while (cursor !== '0');
   }
+  
 
   async execute(): Promise<void> {
     console.log("start")
