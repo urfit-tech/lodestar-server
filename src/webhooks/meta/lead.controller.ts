a6c01e9d709e311400eb0c267eaac268b55afa0e
kwguo <shana0440@gmail.com>
2023-12-01 11:57:35 +0800

    feat: add meta webhook verify endpoint

--- src/webhooks/meta/lead.controller.ts
+++ src/webhooks/meta/lead.controller.ts
@@ -1,15 +1,39 @@
-import { Controller, Get, Req, UseGuards } from '@nestjs/common';
-import { Request } from 'express';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { APIException } from '~/api.excetion';
 
 @Controller({
   path: 'webhooks/meta/lead',
 })
 export class LeadController {
-  constructor() {}
+  constructor(private readonly configService: ConfigService<{ META_VERIFY_TOKEN: string }>) {}
 
   @Get()
-  async ping(@Req() request: Request) {
-    console.log('pong');
+  async verify(
+    @Query('hub.mode') mode: string,
+    @Query('hub.challenge') challenge: string,
+    @Query('hub.verify_token') verifyToken: string,
+  ) {
+    if (verifyToken !== this.configService.get('META_VERIFY_TOKEN')) {
+      throw new APIException(
+        {
+          code: 'E_META_VERIFY',
+          message: 'invalid verify token',
+          result: {
+            mode,
+            challenge,
+            verifyToken,
+          },
+        },
+        400,
+      );
+    }
+
+    return challenge;
+  }
+
+  @Post()
+  async webhook(@Body() body) {
     return 'pong';
   }
 }
