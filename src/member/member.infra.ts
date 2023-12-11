dd9ef161a8f5750b88564221d14c4ddc4355d0ea
kwguo <shana0440@gmail.com>
2023-12-11 14:37:31 +0800

    fix: member property and phone duplicate error

--- src/member/member.infra.ts
+++ src/member/member.infra.ts
@@ -15,7 +15,6 @@ import { Cursor, buildPaginator } from 'typeorm-cursor-pagination';
 import { Injectable } from '@nestjs/common';
 import { first, keys, omit, pick, values } from 'lodash';
 import * as uuid from 'uuid';
-
 import { Member } from './entity/member.entity';
 import { MemberAuditLog } from './entity/member_audit_log.entity';
 import { MemberCategory } from './entity/member_category.entity';
@@ -568,6 +567,50 @@ export class MemberInfrastructure {
       ...data,
     });
     return memberRepo.save(member);
+  }
+  async saveMember(manager: EntityManager, member: Member): Promise<Member> {
+    const memberRepo = manager.getRepository(Member);
+    return memberRepo.save(member);
+  }
+
+  async firstMemberByCondition(manager: EntityManager, conditions: FindOptionsWhere<Member>): Promise<Member | null> {
+    const memberRepo = manager.getRepository(Member);
+    return memberRepo.findOneBy(conditions);
+  }
+
+  async upsertMemberProperty(manager: EntityManager, memberId: string, propertyId: string, value: string) {
+    const memberPropertyRepo = manager.getRepository(MemberProperty);
+    const property = await memberPropertyRepo.findOneBy({
+      memberId,
+      propertyId,
+    });
+    if (property) {
+      property.value = value;
+      return memberPropertyRepo.save(property);
+    }
+
+    const memberProperty = memberPropertyRepo.create({
+      memberId,
+      propertyId,
+      value,
+    });
+    return memberPropertyRepo.save(memberProperty);
+  }
+
+  async upsertMemberPhone(manager: EntityManager, memberId: string, phone: string) {
+    const memberPhoneRepo = manager.getRepository(MemberPhone);
+    const memberPhone = await memberPhoneRepo.findOneBy({
+      memberId,
+      phone,
+    });
+    if (memberPhone) {
+      return memberPhone;
+    }
+    const newMemberPhone = memberPhoneRepo.create({
+      memberId,
+      phone,
+    });
+    return memberPhoneRepo.save(newMemberPhone);
   }
 
   private getMemberPropertyQueryBuilderByCondition(entityManager: EntityManager, conditions: FindOptionsWhere<Member>) {
--- src/webhooks/meta/lead.service.ts
+++ src/webhooks/meta/lead.service.ts
@@ -1,19 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectEntityManager } from '@nestjs/typeorm';
 import { EntityManager } from 'typeorm';
+import * as uuid from 'uuid';
+
 import { AppService } from '~/app/app.service';
 import { DefinitionInfrastructure } from '~/definition/definition.infra';
-import { MemberService } from '~/member/member.service';
 import { LeadWebhookBody } from './lead.dto';
-import { MemberProperty } from '~/member/entity/member_property.entity';
-import { MemberPhone } from '~/member/entity/member_phone.entity';
 import { APIException } from '~/api.excetion';
+import { MemberInfrastructure } from '~/member/member.infra';
+import { Member } from '~/member/entity/member.entity';
 
 @Injectable()
 export class LeadService {
   constructor(
     private readonly appService: AppService,
-    private readonly memberService: MemberService,
+    private readonly memberInfra: MemberInfrastructure,
     private readonly definitionInfra: DefinitionInfrastructure,
     @InjectEntityManager() private readonly entityManager: EntityManager,
   ) {}
@@ -44,35 +45,61 @@ export class LeadService {
         Object.keys(propertyNameToField),
         entityManager,
       );
-      const member = await this.memberService.upsertMemberBy(
-        {
-          appId: app.id,
-          email: body.email,
-          name: body.full_name,
-          username: body.email,
-          role: 'general-member',
-        },
-        {
-          appId: app.id,
-          email: body.email,
-        },
+      const member = await this.upsertMember(appId, {
+        email: body.email,
+        name: body.full_name,
+        username: body.email,
+        role: 'general-member',
+      });
+      await Promise.all(
+        properties.map((property) => {
+          return this.memberInfra.upsertMemberProperty(
+            this.entityManager,
+            member.id,
+            property.id,
+            body[propertyNameToField[property.name]],
+          );
+        }),
       );
-      const memberPropertyRepo = entityManager.getRepository(MemberProperty);
-      const newProperties = properties.map((property) => {
-        const memberProperty = memberPropertyRepo.create({
-          memberId: member.id,
-          propertyId: property.id,
-          value: body[propertyNameToField[property.name]],
-        });
-        return memberProperty;
-      });
-      await memberPropertyRepo.save(newProperties);
-      const phoneRepo = entityManager.getRepository(MemberPhone);
-      const phone = phoneRepo.create({
-        memberId: member.id,
-        phone: body.phone_number,
-      });
-      await phoneRepo.save(phone);
+      await this.memberInfra.upsertMemberPhone(this.entityManager, member.id, body.phone_number);
     });
+  }
+
+  private async upsertMember(
+    appId: string,
+    data: {
+      email: string;
+      name: string;
+      username: string;
+      role: string;
+    },
+  ) {
+    let member = await this.memberInfra.firstMemberByCondition(this.entityManager, {
+      appId,
+      email: data.email,
+    });
+    if (member) {
+      const metadata = member.metadata as { is_assigned?: boolean; from_lead_webhook_at: string };
+      member.metadata = {
+        ...metadata,
+        is_assigned: metadata.is_assigned ?? false,
+        from_lead_webhook_at: metadata.from_lead_webhook_at ?? new Date().toISOString(),
+      };
+    } else {
+      const memberRepo = this.entityManager.getRepository(Member);
+      member = memberRepo.create({
+        id: uuid.v4(),
+        appId,
+        email: data.email,
+        name: data.name,
+        username: data.username,
+        role: data.role,
+        metadata: {
+          is_assigned: false,
+          from_lead_webhook_at: new Date().toISOString(),
+        },
+      });
+    }
+    return this.memberInfra.saveMember(this.entityManager, member);
   }
 }
