195d85440f42b254bb88f914f1489f1d7f9056f4
Logan <clothe09986@gmail.com>
2023-12-19 03:40:17 +0800

    Implement voucher retrieval in the service and controller
    Introduced a new controller and service to handle fetching vouchers for
    members, alongside supportive infrastructure modifications. The
    VoucherController provides an endpoint to fetch vouchers based on member
    IDs, using the newly added VoucherService, which in turn interacts with
    the VoucherInfrastructure for querying the database. Error handling is
    in place for member existence verification. Additionally, the voucher
    module now integrates required modules and services to support these new
    features. Refactoring ensures vouchers' timestamps and usage are
    correctly represented in the response.

---
+++ src/voucher/voucher.controller.ts
@@ -0,0 +1,22 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { JwtMember } from '~/auth/auth.dto';
+import { AuthGuard } from '~/auth/auth.guard';
+import { Local } from '~/decorator';
+import { VoucherService } from './voucher.service';
+
+@UseGuards(AuthGuard)
+@Controller({
+  path: 'vouchers',
+  version: '2',
+})
+export class VoucherController {
+  constructor(private voucherService: VoucherService) {}
+
+  @Get()
+  async getVoucherByMemberId(@Local('member') member: JwtMember, @Req() request: Request) {
+    const { memberId } = request.query;
+
+    return this.voucherService.getVoucherByMemberId(member.appId, String(memberId || member.memberId));
+  }
+}
--- src/voucher/voucher.module.ts
+++ src/voucher/voucher.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
 import { VoucherPlanService } from './voucher-plan/voucher-plan.service';
 import { VoucherInfrastructure } from './voucher.infra';
+import { MemberModule } from '~/member/member.module';
+import { AuthModule } from '~/auth/auth.module';
+import { VoucherService } from './voucher.service';
+import { MemberService } from '~/member/member.service';
+import { DefinitionInfrastructure } from '~/definition/definition.infra';
+import { VoucherController } from './voucher.controller';
+import { OrderInfrastructure } from '~/order/order.infra';
+import { UtilityService } from '~/utility/utility.service';
 
 @Module({
-  providers: [VoucherPlanService, VoucherInfrastructure],
+  controllers: [VoucherController],
+  imports: [AuthModule, MemberModule],
+  providers: [
+    VoucherPlanService,
+    VoucherInfrastructure,
+    VoucherService,
+    MemberService,
+    DefinitionInfrastructure,
+    OrderInfrastructure,
+    UtilityService,
+  ],
   exports: [VoucherInfrastructure],
 })
 export class VoucherModule {}
