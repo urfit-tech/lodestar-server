e37fc5330a8dfc0a80755953dd543ad135bf2220
Logan <clothe09986@gmail.com>
2024-01-29 05:16:06 +0800

    feat:program-package api

--- src/program-package/program-package.infra.ts
+++ src/program-package/program-package.infra.ts
@@ -107,4 +107,68 @@ export class ProgramPackageInfrastructure {
 
     return this.utilityService.convertObjectKeysToCamelCase(programPackages);
   }
+
+  async getEnrolledProgramPackageById(memberId: string, programPackageId: string, manager: EntityManager) {
+    const programPackages = await manager
+      .getRepository(OrderLog)
+      .createQueryBuilder('order_log')
+      .select([
+        'program_package.id AS id',
+        'program_package.title AS title',
+        'program_package.cover_url AS cover_url',
+        'program_package_plan.is_tempo_delivery AS is_tempo_delivery',
+        `JSONB_AGG(DISTINCT JSONB_BUILD_OBJECT('program_id', program.id,'program_title', program.title,'program_cover_url', program.cover_url,'program_content_id', program_content.id, 'progress', program_content_progress.progress ,'program_package_program_id', program_package_program.id,'position', program_package_program.position)) AS program_content_progress`,
+        `JSONB_AGG(DISTINCT program_tempo_delivery.*) AS program_tempo_delivery`,
+      ])
+      .where(`order_log.member_id = :memberId`, { memberId })
+      .andWhere('program_package.id = :programPackageId', { programPackageId })
+      .andWhere('program.published_at IS NOT NULL')
+      .innerJoin(
+        'order_product',
+        'order_product',
+        'order_product.delivered_at < NOW()' +
+          ' AND order_product.order_id = order_log.id' +
+          ' AND (order_product.ended_at IS NULL OR order_product.ended_at > NOW())' +
+          ' AND (order_product.started_at IS NULL OR order_product.started_at <= NOW())',
+      )
+      .innerJoin('product', 'product', 'product.id = order_product.product_id' + ' AND product.type = :productType', {
+        productType: 'ProgramPackagePlan',
+      })
+      .leftJoin('program_package_plan', 'program_package_plan', 'program_package_plan.id::text = product.target')
+      .leftJoin('program_package', 'program_package', 'program_package.id = program_package_plan.program_package_id')
+      .leftJoin(
+        'program_package_program',
+        'program_package_program',
+        'program_package_program.program_package_id = program_package.id',
+      )
+      .leftJoin('program', 'program', 'program.id = program_package_program.program_id')
+      .leftJoin('program_content_section', 'program_content_section', 'program_content_section.program_id = program.id')
+      .leftJoin(
+        'program_content',
+        'program_content',
+        'program_content.content_section_id = program_content_section.id' +
+          ' AND program_content.published_at IS NOT NULL',
+      )
+      .leftJoin(
+        'program_content_progress',
+        'program_content_progress',
+        'program_content_progress.program_content_id = program_content.id' +
+          ' AND program_content_progress.member_id = :memberId',
+        { memberId },
+      )
+      .leftJoin(
+        'program_tempo_delivery',
+        'program_tempo_delivery',
+        'program_tempo_delivery.program_package_program_id = program_package_program.id' +
+          ' AND program_tempo_delivery.member_id = :memberId',
+        {
+          memberId,
+        },
+      )
+      .groupBy('program_package.id')
+      .addGroupBy('program_package_plan.id')
+      .getRawOne();
+
+    return this.utilityService.convertObjectKeysToCamelCase(programPackages);
+  }
 }
--- src/program-package/program-package.service.ts
+++ src/program-package/program-package.service.ts
@@ -6,6 +6,9 @@ import { OrderLog } from '~/order/entity/order_log.entity';
 import { MemberService } from '~/member/member.service';
 import { APIException } from '~/api.excetion';
 import { ProgramPackageInfrastructure } from './program-package.infra';
+import { ProgramPackageProgramContentProgressDTO, ProgramTempoDeliveryDTO } from './program-package.dto';
+import { isEmpty } from 'lodash';
+import { ProgramInfrastructure } from '~/program/program.infra';
 
 @Injectable()
 export class ProgramPackageService {
@@ -13,6 +16,7 @@ export class ProgramPackageService {
     private readonly memberService: MemberService,
     private readonly programPackageInfra: ProgramPackageInfrastructure,
     @InjectEntityManager() private readonly entityManager: EntityManager,
+    private readonly programInfra: ProgramInfrastructure,
   ) {}
 
   public async getProgramPackageByMemberId(appId: string, memberId: string) {
@@ -66,5 +70,101 @@ export class ProgramPackageService {
         })),
       ),
     ];
+  }
+
+  public async getEnrolledProgramPackageById(appId: string, memberId: string, programPackageId: string) {
+    // Todo: check permission
+    // ...
+
+    const { data: memberData } = await this.memberService.getMembersByCondition(appId, { limit: 1 }, { id: memberId });
+    if (memberData.length === 0) {
+      throw new APIException({
+        code: 'E_NO_MEMBER',
+        message: 'member not found',
+        result: null,
+      });
+    }
+
+    const ownedProgramPackages = await this.programPackageInfra.getEnrolledProgramPackageById(
+      memberId,
+      programPackageId,
+      this.entityManager,
+    );
+
+    const filterProgramContentProgress = ownedProgramPackages.isTempoDelivery
+      ? this.filterProgramContentProgressNotTempoDelivered(
+          ownedProgramPackages.programContentProgress,
+          ownedProgramPackages.programTempoDelivery.filter((delivery) => !isEmpty(delivery)),
+        )
+      : ownedProgramPackages.programContentProgress;
+
+    const programCategories = await this.programInfra.getProgramCategories(
+      filterProgramContentProgress.map((p: ProgramPackageProgramContentProgressDTO) => p.programId),
+      this.entityManager,
+    );
+
+    const progress = await this.calculateProgramPackageProgramProgress(filterProgramContentProgress);
+
+    return {
+      id: ownedProgramPackages.id,
+      title: ownedProgramPackages.title,
+      coverUrl: ownedProgramPackages.coverUrl,
+      programs: [
+        ...new Set(filterProgramContentProgress.sort((a, b) => a.position - b.position).map((p) => p.programId)),
+      ].map((programId: string) => ({
+        id: programId,
+        title: filterProgramContentProgress.find((p) => p.programId === programId).programTitle || '',
+        coverUrl: filterProgramContentProgress.find((p) => p.programId === programId).programCoverUrl || null,
+        viewRate: progress[programId],
+        categories:
+          programCategories
+            .find((pc) => pc.id === programId)
+            ?.programCategories.map((programCategory) => ({
+              id: programCategory.category.id,
+              name: programCategory.category.name,
+              position: programCategory.category.position,
+            })) || [],
+      })),
+    };
+  }
+
+  private filterProgramContentProgressNotTempoDelivered(
+    programContentProgress: ProgramPackageProgramContentProgressDTO[],
+    programTempoDelivery: ProgramTempoDeliveryDTO[],
+  ) {
+    return programContentProgress.filter((progress) =>
+      programTempoDelivery.some(
+        (delivery) =>
+          delivery.programPackageProgramId === progress.programPackageProgramId &&
+          new Date(delivery.deliveredAt).getTime() < Date.now(),
+      ),
+    );
+  }
+
+  private async calculateProgramPackageProgramProgress(
+    programContentProgress: ProgramPackageProgramContentProgressDTO[],
+  ) {
+    const progressSummary = programContentProgress.reduce((acc, item) => {
+      if (!acc[item.programId]) {
+        acc[item.programId] = { sum: 0, count: 0 };
+      }
+
+      acc[item.programId].sum += item.progress;
+      acc[item.programId].count += 1;
+
+      return acc;
+    }, {});
+
+    const averageProgress = {};
+
+    for (const id in progressSummary) {
+      if (progressSummary[id].count > 0) {
+        averageProgress[id] = Math.floor((progressSummary[id].sum / progressSummary[id].count) * 100) / 100;
+      } else {
+        averageProgress[id] = null;
+      }
+    }
+
+    return averageProgress;
   }
 }
