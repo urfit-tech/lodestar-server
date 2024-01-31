59ed858986e052bd071b4f235aa56cdff5d02956
Ted <wodf410@gmail.com>
2024-01-31 18:22:42 +0800

    feat: trial ebook auth

--- src/media/ebook/ebook.controller.ts
+++ src/media/ebook/ebook.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Param, Post, Req, Res, UseGuards, Headers, UnauthorizedException } from '@nestjs/common';
+import { Controller, Get, Param, Req, Res, Headers, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '../../auth/auth.guard';
 import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 import { APIException } from '~/api.excetion';
@@ -28,53 +28,64 @@ export class EbookController {
   @Get(':programContentId')
   public async getStandardEbook(
     @Param('programContentId') programContentId: string,
-    @Headers('Authorization') authorization: string,
-    @Local('member') member: JwtMember,
     @Req() req: Request,
-    @Res() res: Response
+    @Res() res: Response,
+    @Headers('Authorization') authorization?: string
   ): Promise<void> {
+    const contentId = programContentId.split(".")[0];
+    let programContents;
+
     try {
-      const [contentId, ..._] = programContentId.split(".");
-      const programContents = await this.programService.getProgramContentById(contentId);
+      programContents = await this.programService.getProgramContentById(contentId);
+    } catch (err) {
+      throw new APIException({ code: 'E_EBOOK_NOT_FOUND', message: 'Unable to retrieve ebook file' }, 400)
+    }
 
-      const isTrial = programContents?.displayMode === 'trial';
-      const appId = isTrial ? 'trial' : this.extractAppId(authorization);
+    const isTrial = !authorization && programContents.displayMode === 'trial';
+    const appId = programContents.contentSection.program.appId;
 
-      await this.processGetEbook(appId, programContentId, req, res, isTrial);
-    } catch (error) {
+    if (authorization) {
+      this.verifyAuthorization(authorization);
+    } else if (!isTrial) {
+      throw new UnauthorizedException();
+    }
+
+    try {
+      const encryptedFileStream = await this.ebookService.processEbook(appId, programContentId, req, isTrial);
+
+      res.setHeader('Content-Type', 'application/epub+zip');
+      res.setHeader('Content-Disposition', `attachment; filename="${programContentId}.epub"`);
+
+      encryptedFileStream.pipe(res);
+
+    } catch(error) {
       this.handleError(error, res);
     }
   }
 
-  private extractAppId(authorization: string): string {
+  private verifyAuthorization(authorization: string) {
     const token = authorization.split(' ')[1];
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('No token provided');
     }
-    return this.authService.verify(token).appId;
+    
+    try {
+      const verifiedToken = this.authService.verify(token);
+      if (!verifiedToken) {
+        throw new UnauthorizedException('Invalid or expired token');
+      }
+      return verifiedToken;
+    } catch (error) {
+      throw new UnauthorizedException('Invalid token');
+    }
   }
 
-  private async processGetEbook(
-    appId: string,
-    programContentId: string,
-    req: Request,
-    res: Response,
-    isTrial: boolean
-  ): Promise<void> {
-    const encryptedFileStream = await this.ebookService.processEbook(appId, programContentId, req, isTrial);
-
-    res.setHeader('Content-Type', 'application/epub+zip');
-    res.setHeader('Content-Disposition', `attachment; filename="${programContentId}.epub"`);
-    encryptedFileStream.pipe(res);
-  }
 
   private handleError(error: any, res: Response): void {
     if (error instanceof EbookFileRetrievalError || error instanceof KeyAndIVRetrievalError || error instanceof EbookEncryptionError) {
-      throw new APIException({ code: `E_${error.name.toUpperCase()}`, message: error.message }, 400);
-    } else if (error instanceof UnauthorizedException) {
-      throw error;
+      res.status(400).json(new APIException({ code: `E_${error.name.toUpperCase()}`, message: error.message }));
     } else {
-      throw new APIException({ code: 'E_UNKNOWN', message: 'Unknown error' }, 500);
+      res.status(500).json(new APIException({ code: 'E_UNKNOWN', message: 'Unknown error' }));
     }
   }
 }
