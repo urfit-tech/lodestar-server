42ff5193f60a555bad22710a9ee33c89fd7d64ff
Ted <wodf410@gmail.com>
2024-02-22 11:08:36 +0800

    feat: Add InvoiceGateway entity and column to PaymentLog entity

--- migrations/hasura/migrations/default/1680330156157_Init/up.sql
+++ migrations/hasura/migrations/default/1680330156157_Init/up.sql
@@ -8182,6 +8182,30 @@ EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
 COMMENT ON TRIGGER "set_public_program_content_ebook_updated_at" ON "public"."program_content_ebook"
 IS 'trigger to set value of column "updated_at" to current timestamp on row update';
 alter table "public"."program_content_ebook" rename column "attachment_id" to "program_content_id";
+CREATE TABLE "public"."invoice_gateway" ("id" uuid NOT NULL DEFAULT gen_random_uuid(), "name" text NOT NULL, "created_at" timestamp with time zone NOT NULL DEFAULT now(), "updated_at" timestamp with time zone NOT NULL DEFAULT now(), PRIMARY KEY ("id") , UNIQUE ("id"));
+CREATE OR REPLACE FUNCTION "public"."set_current_timestamp_updated_at"()
+RETURNS TRIGGER AS $$
+DECLARE
+  _new record;
+BEGIN
+  _new := NEW;
+  _new."updated_at" = NOW();
+  RETURN _new;
+END;
+$$ LANGUAGE plpgsql;
+CREATE TRIGGER "set_public_invoice_gateway_updated_at"
+BEFORE UPDATE ON "public"."invoice_gateway"
+FOR EACH ROW
+EXECUTE PROCEDURE "public"."set_current_timestamp_updated_at"();
+COMMENT ON TRIGGER "set_public_invoice_gateway_updated_at" ON "public"."invoice_gateway"
+IS 'trigger to set value of column "updated_at" to current timestamp on row update';
+
+alter table "public"."payment_log" add column "invoice_gateway_id" uuid
+ null;
+
+
+
+
 
 
 
