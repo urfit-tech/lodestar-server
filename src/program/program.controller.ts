78723e00c60f4d114c7c111a2fb6c8e51c6836fc
loischen68 <a8283626@gmail.com>
2024-02-23 16:09:39 +0800

    feat: program content permission

--- src/program/program.controller.ts
+++ src/program/program.controller.ts
@@ -35,14 +35,18 @@ export class ProgramController {
     @Param('programContentId') programContentId: string,
   ) {
     const { memberId } = request.query;
+    const { role, permissions } = member;
 
-    return member.role === 'app-owner'
+    const extraAllowPermission = ['PROGRAM_NORMAL'].find((e) => permissions.includes(e));
+
+    return role === 'app-owner' || ['PROGRAM_ADMIN'].some((e) => permissions.includes(e))
       ? { programContentId }
       : this.programService.getEnrolledProgramContentById(
           member.appId,
           String(memberId || member.memberId),
           programId,
           programContentId,
+          extraAllowPermission,
         );
   }
 
