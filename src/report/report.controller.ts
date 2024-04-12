b4d37f5d49435bd4e0a1f515d94567963dfff426
Logan <clothe09986@gmail.com>
2024-04-12 23:19:24 +0800

    feat: add report config options

--- src/report/report.controller.ts
+++ src/report/report.controller.ts
@@ -21,27 +21,28 @@ export class ReportController {
     const report: GetReportDTO = await this.reportService.getReportById(reportId);
     const { type, options } = report;
     const { appId, memberId, role } = member;
-
     let result;
     switch (type) {
       case 'metabase':
-        const payload = !!Object.keys(options.metabase.resource).includes('dashboard')
-          ? {
-              ...options.metabase,
-              params: {
-                appid: appId,
-                memberid: memberId,
-                role,
-              },
-            }
-          : {
-              ...options.metabase,
-              params: {
-                appId,
-                memberId,
-                role,
-              },
-            };
+        const payload = options.canViewSelfDataOnly
+          ? !!Object.keys(options.metabase.resource).includes('dashboard')
+            ? {
+                ...options.metabase,
+                params: {
+                  appid: appId,
+                  memberid: memberId,
+                  role,
+                },
+              }
+            : {
+                ...options.metabase,
+                params: {
+                  appId,
+                  memberId,
+                  role,
+                },
+              }
+          : options.metabase;
 
         result = this.reportService.generateMetabaseSignedUrl(payload);
         break;
