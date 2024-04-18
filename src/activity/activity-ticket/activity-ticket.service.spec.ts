23650efec1276766585f8c1fe72b8d52dc36d9ed
Ted <wodf410@gmail.com>
2024-04-18 16:58:41 +0800

    feat: update activity-ticket unit test

--- src/activity/activity-ticket/activity-ticket.service.spec.ts
+++ src/activity/activity-ticket/activity-ticket.service.spec.ts
@@ -1,18 +1,38 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ActivityTicketService } from './activity-ticket.service';
+import { ActivityTicketInfrastructure } from './activity-ticket.infra';
+import { EntityManager } from 'typeorm';
+import { getEntityManagerToken } from '@nestjs/typeorm';
 
 describe('ActivityTicketService', () => {
   let service: ActivityTicketService;
+  let manager: EntityManager;
+
+  const mockActivityTicketInfra = {
+    getActivityTicketInfoByIdAndMemberId: jest.fn(),
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ActivityTicketService],
+      providers: [
+        ActivityTicketService,
+        {
+          provide: ActivityTicketInfrastructure,
+          useValue: mockActivityTicketInfra
+        },
+        {
+          provide: getEntityManagerToken(),
+          useValue: jest.fn() 
+        }
+      ],
     }).compile();
 
     service = module.get<ActivityTicketService>(ActivityTicketService);
+    manager = module.get<EntityManager>(getEntityManagerToken());
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
 });
+
