93f7a1742756e2b92f2a2e61aa41836e6847b7f8
Ted <wodf410@gmail.com>
2024-04-29 23:56:28 +0800

    test: add cached events are not found in calender service unit test

--- src/calendar/calendar.service.spec.ts
+++ src/calendar/calendar.service.spec.ts
@@ -15,17 +15,13 @@ describe('CalendarService', () => {
   beforeEach(async () => {
     mockGet = jest.fn();
     mockSet = jest.fn();
-
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         CalendarService,
         {
           provide: CacheService,
           useValue: {
-            getClient: jest.fn(() => ({
-              get: mockGet,
-              set: mockSet,
-            })),
+            getClient: jest.fn(() => ({ get: mockGet, set: mockSet })),
           },
         },
         {
@@ -53,12 +49,67 @@ describe('CalendarService', () => {
     const memberId = '123';
     const mockEvents = [{ uid: 'task1', start: [2023, 5, 17, 10, 0], title: 'Test Task' }];
     mockGet.mockResolvedValue(JSON.stringify(mockEvents));
-
     const result = await service.getCalendarEventsByMemberId(memberId);
-
     expect(result).toEqual(mockEvents);
     expect(mockGet).toHaveBeenCalledWith(`calendar_${memberId}`);
     expect(memberService.getMemberTasksByExecutorId).not.toHaveBeenCalled();
     expect(appointmentService.getAppointmentEnrollmentByCreatorId).not.toHaveBeenCalled();
+  });
+
+  it('should call getAppointmentEnrollmentByCreatorId when cached events are not found and tasks are empty', async () => {
+    const memberId = '123';
+    mockGet.mockResolvedValue(null);
+
+    const getMemberTasksByExecutorIdMock = jest.fn().mockResolvedValue([]);
+    memberService.getMemberTasksByExecutorId = getMemberTasksByExecutorIdMock;
+
+    const mockAppointments = [
+      {
+        uid: 'appointment1',
+        start: [2023, 4, 17, 10, 0],
+        end: [2023, 4, 17, 11, 0],
+        title: 'Test Appointment',
+        description: 'This is a test appointment',
+      },
+    ];
+
+    const getAppointmentEnrollmentByCreatorIdMock = jest.fn().mockResolvedValue(
+      mockAppointments.map((appointment) => ({
+        orderProductId: appointment.uid,
+        startedAt: new Date(
+          appointment.start[0],
+          appointment.start[1],
+          appointment.start[2],
+          appointment.start[3],
+          appointment.start[4],
+        ),
+        endedAt: new Date(
+          appointment.end[0],
+          appointment.end[1],
+          appointment.end[2],
+          appointment.end[3],
+          appointment.end[4],
+        ),
+        orderProductName: appointment.title,
+        order_product_description: appointment.description,
+      })),
+    );
+    appointmentService.getAppointmentEnrollmentByCreatorId = getAppointmentEnrollmentByCreatorIdMock;
+
+    const result = await service.getCalendarEventsByMemberId(memberId);
+
+    expect(result).toEqual([
+      {
+        uid: 'appointment1',
+        start: [2023, 5, 17, 10, 0], // please note that the month here is 5 because months in JavaScript are zero-indexed
+        end: [2023, 5, 17, 11, 0],
+        title: 'Test Appointment',
+        description: 'This is a test appointment',
+      },
+    ]);
+
+    expect(mockGet).toHaveBeenCalledWith(`calendar_${memberId}`);
+    expect(memberService.getMemberTasksByExecutorId).toHaveBeenCalledWith(memberId);
+    expect(appointmentService.getAppointmentEnrollmentByCreatorId).toHaveBeenCalledWith(memberId);
   });
 });
