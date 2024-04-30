3629f8a1fbfac8ad491b9ecf389affca9a6acd0a
ponchimeow <trancetoradora@gmail.com>
2024-04-30 14:49:05 +0800

    fix: equity module providers

--- src/equity/equity.module.ts
+++ src/equity/equity.module.ts
@@ -5,10 +5,24 @@ import { DefinitionModule } from '~/definition/definition.module';
 import { ActivityTicketInfrastructure } from '~/activity/activity-ticket/activity-ticket.infra';
 import { ActivityTicketService } from '~/activity/activity-ticket/activity-ticket.service';
 import { ActivityModule } from '~/activity/activity.module';
+import { ProgramService } from '~/program/program.service';
+import { MemberService } from '~/member/member.service';
+import { ProgramInfrastructure } from '~/program/program.infra';
+import { UtilityService } from '~/utility/utility.service';
+import { MemberInfrastructure } from '~/member/member.infra';
 
 @Module({
   controllers: [EquityController],
-  imports: [DefinitionModule, forwardRef(()=> AuthModule), ActivityModule],
-  providers: [Logger, ActivityTicketInfrastructure, ActivityTicketService]  
+  imports: [DefinitionModule, forwardRef(() => AuthModule), ActivityModule],
+  providers: [
+    Logger,
+    ActivityTicketInfrastructure,
+    ProgramInfrastructure,
+    MemberInfrastructure,
+    ActivityTicketService,
+    ProgramService,
+    MemberService,
+    UtilityService,
+  ],
 })
 export class EquityModule {}
