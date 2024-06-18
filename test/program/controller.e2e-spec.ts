2b50cdd57f44fc49ac675d658ce738e9aab1a9e0
Ted <wodf410@gmail.com>
2024-06-18 12:37:54 +0800

    test: remove only

--- test/program/controller.e2e-spec.ts
+++ test/program/controller.e2e-spec.ts
@@ -2106,7 +2106,7 @@ describe('ProgramController (e2e)', () => {
     });
   });
 
-  describe.only('POST /:programId/content/:contentId/track-process', () => {
+  describe('POST /:programId/content/:contentId/track-process', () => {
     beforeEach(async () => {
       await programContentProgressRepo.delete({});
     });
