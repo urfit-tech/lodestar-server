aa6ebb6b943474e1b8dc114beb0492a6e5a26f2f
Logan <clothe09986@gmail.com>
2024-08-30 03:56:08 +0800

    feat: invoice issue/search/revoke

--- src/invoice/invoice.dto.ts
+++ src/invoice/invoice.dto.ts
@@ -1,6 +1,61 @@
-import { IsString } from 'class-validator';
+import { IsObject, IsString } from 'class-validator';
 
 export class IssueInvoiceBodyDTO {
   @IsString()
-  paymentNo: string;
+  appId: string;
+
+  @IsString()
+  invoiceGatewayId: string;
+
+  @IsObject()
+  invoiceInfo: {
+    MerchantOrderNo: string;
+    BuyerName: string;
+    BuyerUBN?: string;
+    BuyerAddress?: string;
+    BuyerPhone?: string;
+    BuyerEmail?: string;
+    Category: string;
+    TaxType: string;
+    TaxRate: string;
+    Amt: string;
+    TaxAmt: string;
+    TotalAmt: string;
+    LoveCode?: string;
+    PrintFlag: string;
+    ItemName: string;
+    ItemCount: string;
+    ItemUnit: string;
+    ItemPrice: string;
+    ItemAmt: string;
+    ItemTaxType?: string;
+    Comment?: string;
+  };
+}
+
+export class SearchInvoiceBodyDTO {
+  @IsString()
+  invoiceGatewayId: string;
+
+  @IsString()
+  invoiceNumber: string;
+
+  @IsString()
+  appId: string;
+
+  @IsString()
+  invoiceRandomNumber: string;
+}
+export class RevokeInvoiceBodyDTO {
+  @IsString()
+  invoiceGatewayId: string;
+
+  @IsString()
+  invoiceNumber: string;
+
+  @IsString()
+  appId: string;
+
+  @IsString()
+  invalidReason: string;
 }
