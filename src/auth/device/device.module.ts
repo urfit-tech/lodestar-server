6c482b6946a020e854ee9a20af42cf54488ec7f5
ponchimeow <trancetoradora@gmail.com>
2025-02-16 17:39:11 +0800

    feat: device limit logout feature

--- src/auth/device/device.module.ts
+++ src/auth/device/device.module.ts
@@ -1,11 +1,12 @@
-import { Module } from '@nestjs/common';
-
+import { Logger, Module } from '@nestjs/common';
 import DeviceService from './device.service';
 import { DeviceInfrastructure } from './device.infra';
 import { MemberInfrastructure } from '~/member/member.infra';
+import { DeviceController } from './device.controller';
 
 @Module({
-  providers: [DeviceService, DeviceInfrastructure, MemberInfrastructure],
+  controllers: [DeviceController],
+  providers: [Logger, DeviceService, DeviceInfrastructure, MemberInfrastructure],
   exports: [DeviceService, DeviceInfrastructure],
 })
 export class DeviceModule {}
