b36a1d5fed51cbc5638486ccd5489951d7eb3df1
HsuPeiChun <philosophysis@gmail.com>
2025-04-21 15:09:32 +0800

    bugfix: change CarrierType from number to string

--- src/invoice/invoice.service.ts
+++ src/invoice/invoice.service.ts
@@ -373,7 +373,7 @@ export class InvoiceService {
     } else if (options.phoneBarCode) {
       invoiceAttrs = {
         Category: 'B2C',
-        CarrierType: 0,
+        CarrierType: '0',
         CarrierNum: options.phoneBarCode,
         PrintFlag: 'N',
       };
@@ -387,7 +387,7 @@ export class InvoiceService {
     } else if (options.citizenCode) {
       invoiceAttrs = {
         Category: 'B2C',
-        CarrierType: 1,
+        CarrierType: '1',
         CarrierNum: options.citizenCode,
         PrintFlag: 'N',
       };
@@ -395,7 +395,7 @@ export class InvoiceService {
       invoiceAttrs = {
         Category: 'B2C',
         PrintFlag: 'N',
-        CarrierType: 2,
+        CarrierType: '2',
         KioskPrintFlag: '1',
       };
     } else if (options.email) {
