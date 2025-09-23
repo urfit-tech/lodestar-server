3a37f0f7db6d22c910b112b3d1bb843b54998505
Eddy <eddy84512a@gmail.com>
2025-09-23 14:48:36 +0800

    fix: remove unnecessary file

--- src/meta/data-deletion/data-deletion.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { DataDeletionController } from './data-deletion.controller';
-
-@Module({
-  controllers: [DataDeletionController],
-  providers: [],
-  exports: [],
-})
-export class DataDeletionModule {}
